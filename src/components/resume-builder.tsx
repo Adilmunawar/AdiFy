dfeba5c7e671d502d12978f32ea659f13d33bee6
Adil Munawar <adilfromavengers@gmail.com>
2025-09-05 22:36:59 +0000

    as you can see from the image that I have to scroll down to see what's a

--- src/components/resume-builder.tsx
+++ src/components/resume-builder.tsx
@@ -8,11 +8,11 @@ export default function ResumeBuilder() {
   return (
     <div className="flex-1 grid grid-cols-1 md:grid-cols-2 h-[calc(100vh-65px)]">
       <ResumeForm />
-      <ScrollArea className="h-full bg-secondary/50 no-print">
-        <div id="resume-container" className="p-4 sm:p-8 flex items-center justify-center">
+      <div className="h-full bg-secondary/50 no-print overflow-y-auto">
+        <div id="resume-container" className="p-4 sm:p-8 flex justify-center">
             <ResumePreview />
         </div>
-      </ScrollArea>
+      </div>
     </div>
   );
 }
