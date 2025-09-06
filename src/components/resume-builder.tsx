48b112d41b2d27c1aa1f090d800c9207677074ff
Adil Munawar <adilfromavengers@gmail.com>
2025-09-06 07:00:07 +0000

    no it's still blank I

--- src/components/resume-builder.tsx
+++ src/components/resume-builder.tsx
@@ -7,7 +7,7 @@ export default function ResumeBuilder() {
   return (
     <div className="flex-1 grid grid-cols-1 md:grid-cols-2 h-[calc(100vh-65px)]">
       <ResumeForm />
-      <div className="h-full bg-secondary/50 no-print overflow-hidden flex justify-center items-start">
+      <div className="h-full bg-secondary/50 overflow-hidden flex justify-center items-start">
         <div id="resume-container" className="py-8">
             <ResumePreview />
         </div>
