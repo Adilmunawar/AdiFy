6946e748f63cb6347c8147e892baa2dcac15f39b
Adil Munawar <adilfromavengers@gmail.com>
2025-09-06 06:24:35 +0000

    you have to add such professional component on the home page top. here i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,22 @@ import Header from "@/components/landing-page/header";
 import Hero from "@/components/landing-page/hero";
 import ProvenTemplates from "@/components/landing-page/proven-templates";
 import Testimonials from "@/components/landing-page/testimonials";
+import TrueFocus from "@/components/true-focus";
 import TrustedBy from "@/components/landing-page/trusted-by";
+import '@/components/true-focus.css';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
+        <TrueFocus 
+          sentence="Adify Resume Builder"
+          manualMode={false}
+          blurAmount={5}
+          animationDuration={0.5}
+          pauseBetweenAnimations={1}
+        />
         <Hero />
         <TrustedBy />
         <Testimonials />
